fa229cc8de854ceea5dfae61ff647a909a5e0a75
Krishna sai Patel <patel@Krishnas-MacBook-Air.local>
2020-05-20 17:01:15 +0530

    ios configured

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget  {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
